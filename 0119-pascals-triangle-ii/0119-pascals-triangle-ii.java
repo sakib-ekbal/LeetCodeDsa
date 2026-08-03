bfe38e6f3bb543a7bba0921173d5900fc687248c
sakib-ekbal <sakibekbal1234@gmail.com>
2026-08-03 19:59:50 +0530

    Time: 1 ms (70.55%), Space: 42.5 MB (73.16%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> ans = new ArrayList<>();
+        ans.add(1);
+        for(int i=1;i<=rowIndex;i++){
+            ans.add(1);
+            for(int j=i-1;j>0;j--){
+                ans.set(j,ans.get(j)+ans.get(j-1));
+            }
+        }
+        return ans;    
+    }
+}
